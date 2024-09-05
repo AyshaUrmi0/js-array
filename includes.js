518751eb00f2cb16d1b33657623e6db07d4c8a0e
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-05 12:47:04 +0900

    includes

---
+++ includes.js
@@ -0,0 +1,10 @@
+// Checking Array Membership with ‘includes’
+ 
+
+let books=["java","c","c++","python","javascript"];
+if(books.includes("javascript")){
+    console.log("present");
+}
+else{
+    console.log("not present");
+}
