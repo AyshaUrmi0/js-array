d1fb83b1615ed7b1fd1c580eeaab42b817a030e8
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-05 13:08:55 +0900

    isArray added

---
+++ isArray.js
@@ -0,0 +1,18 @@
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+
+
+let arr1=['mango','banana','apple','orange','grapes'];
+
+console.log(Array.isArray(arr1));
+
+let arr2='[]';
+
+console.log(Array.isArray(arr2));
+
+let arr3=10;
+
+console.log(Array.isArray(arr3))
